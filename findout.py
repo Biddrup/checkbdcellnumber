17bbf9c7f1297f05e45ae99750836037dda82d16
Biddrup Kumar Mallick <Biddrup@users.noreply.github.com>
2016-11-10 23:59:36 +0600

    Update findout.py
    Going on

--- findout.py
+++ findout.py
@@ -1,10 +1,12 @@
 number = input()
 
-if number[0:3] == '880':
+def check(cellnumber):
     for i in range(3, 13):
-        if not number[i].isdecimal():
+        if not cellnumber[i].isdecimal():
             print('This is not')
     else:
         print('Yes, this is')
+if number[0:3] == '880':
+
 else:
     print('How this one')
