c338ac617fb4db201a5bcb7320ee2225a1cf2ca8
Biddrup Kumar Mallick <Biddrup@users.noreply.github.com>
2016-11-14 22:59:44 +0600

    Update checkcellnumber.py
    Will update this one later more

--- checkcellnumber.py
+++ checkcellnumber.py
@@ -1,22 +1,25 @@
 def isphonenumber(text):
+    #The number should be 12 digit
     if len(text) != 12:
-        return False
+        return 'Cell number should be 10 digit'
     for i in range(0, 3):
         if not text[i].isdecimal():
-            return False
+            return 'First, 3 digit is only number'
     if text[3] != '-':
-        return False
+        return "Please, give a '-' after first three digit"
     for i in range(4, 7):
         if not text[i].isdecimal():
-            return False
+            return 'Second, 3 digit is only number'
     if text[7] != '-':
-        return False
+        return "Give a '-' after second 3 digits"
     for i in range(8, 12):
         if not text[i].isdecimal():
-            return False
-    return True
-print('Please, give me a number')
+            return 'Last will be 4 digits'
+    return 'You have putted a correct cell number'
+
+#Will take a cell number from you
+print('Please, give me a cell number with 10 digits (Ex-111-222-3333)')
 user = input()
 
+#Check the number via this
 print(isphonenumber(user))
-
